cd17c4f6dd1cc346125ee8bcdb76c628257e58ed
Mukesh8354 <mukeshreck907@gmail.com>
2025-12-10 09:50:52 +0530

    Fix online changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header(
     "Access-Control-Allow-Origin",
-    "https://remarkable-sprinkles-2cf9b8.netlify.app"
+    "https://storied-starship-815a9e.netlify.app"
   );
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
